853f16404c9eecdb7d7ae57b8c75058eb484c2bb
Hans Westerbeek <hans@enhance-ict.com>
2022-10-17 12:39:48 +0200

    unsorted netto

--- src/ctr.clj
+++ src/ctr.clj
@@ -1,6 +1,7 @@
 (ns ctr
   (:require [clojure.data.csv :as csv]
-           [clojure.java.io :as io])
+            [clojure.java.io :as io]
+            [clojure.pprint :as pprint])
   )
 
 (defn csv-data->maps [csv-data]
@@ -21,25 +22,42 @@
    :25 19,
    :26 20])
 
-(defn scores [driver rclass facts]
-  ; returns a list of scores like [25 18 23] in descending order
-  (sort >
+(defn drivers-by-class
+  [rclass facts]
+  (set
     (map
-      (fn [row]
-        (:points row))
+      #(:driver %)
       (filter
-        #(and
-           (= (:driver %) driver)
-           (= (:rclass %) rclass)
-           )
-        facts))))
+        (fn [row] (= rclass (:rclass row)))
+        facts)
+      )))
+
+(defn scores
+  [driver rclass facts]
+  ; returns a list of scores like [25 18 23] in descending order
+  (let [
+        raw (sort >
+                  (map
+                    (fn [row]
+                      (:points row))
+                    (filter
+                      #(and
+                         (= (:driver %) driver)
+                         (= (:rclass %) rclass)
+                         )
+                      facts)))
+        bruto (reduce + raw)
+        ]
+    {:bruto bruto
+     :by-race raw}
+    )
+  )
 
 
 (defn -main
   []
   ; for every class, gather every driver
   ; for every combination of driver and class, gather their results
-
   (with-open [reader (io/reader (io/resource "results.csv"))]
     (let [raw (csv-data->maps (csv/read-csv reader))
           facts (map
@@ -49,12 +67,20 @@
                       :rclass (keyword (:rclass row))
                       })
                      raw)
-          ;r-classes (map #(keyword %) classes)
-          drivers (set (map :driver facts))]
+          r-classes (map #(keyword %) (set (map :rclass facts)))]
 
-      (doseq [r-class [:boxc]]
-        (println (str "Klasse: " r-class))
-        (doseq [driver drivers]
-          (let
-            [driver-scores (scores driver r-class facts)]
-            (println (str driver ": " driver-scores))))))))
+      (doseq [r-class r-classes]
+        (println (str "Klasse: " r-class ))
+        (println "############################")
+        (let [
+              drivers (drivers-by-class r-class facts)
+              ;scores (map #({:driver % :netto (:netto (scores % r-class facts))}) drivers)
+              ;sorted (into (sorted-map-by (fn [key1 key2] (compare (key2 scores) (key1 scores)))) scores)
+              ]
+          (doseq [driver drivers]
+            (let
+              [driver-scores (scores driver r-class facts)]
+
+              (println (str driver " " (format "%03d" (:bruto driver-scores)) " " (:by-race driver-scores)))
+              )))))))
+
