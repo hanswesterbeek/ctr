f7cf2a45462f9d9d64f5b83f999ca626ad407999
Hans Westerbeek <hans@enhance-ict.com>
2022-10-17 15:46:17 +0200

    include scraps

--- src/ctr.clj
+++ src/ctr.clj
@@ -1,7 +1,6 @@
 (ns ctr
   (:require [clojure.data.csv :as csv]
-            [clojure.java.io :as io]
-            [clojure.pprint :as pprint])
+            [clojure.java.io :as io])
   )
 
 (defn csv-data->maps [csv-data]
@@ -11,16 +10,23 @@
             repeat)
        (rest csv-data)))
 
-(def races-to-count-by-num-participations
-  [:13 13, :14 13, :15 13, :16 13 :17 13
-   :18 14,
-   :19 15,
-   :20 16, :21 16,
-   :22 17,
-   :23 18,
-   :24 18,
-   :25 19,
-   :26 20])
+(def scrap-reference {
+                      13 13, 14 13, 15 13, 16 13 17 13
+           18 14,
+           19 15,
+           20 16, 21 16,
+           22 17,
+           23 18,
+           24 18,
+           25 19,
+           26 20})
+
+(defn races-to-count-by-num-starts
+  [actual-starts]
+  (if
+    (< actual-starts (first (sort < (keys scrap-reference))))
+      actual-starts
+      (get scrap-reference actual-starts)))
 
 (defn drivers-by-class
   [rclass facts]
@@ -46,18 +52,24 @@
                      )
                   facts)))
         bruto (reduce + raw)
-        ]
+        num-races (count raw)
+        num-to-consider (races-to-count-by-num-starts num-races)
+        relevant-results (take num-to-consider raw)
+        netto (reduce + relevant-results)]
     {:driver driver
      :bruto bruto
-     :by-race raw}
+     :by-race raw
+     :total-num-races num-races
+     :num-counting-races num-to-consider
+     :relevant-results relevant-results
+     :netto netto
+     }
     )
   )
 
 
 (defn -main
   []
-  ; for every class, gather every driver
-  ; for every combination of driver and class, gather their results
   (with-open [reader (io/reader (io/resource "results.csv"))]
     (let [raw (csv-data->maps (csv/read-csv reader))
           facts (map
@@ -68,15 +80,15 @@
                       })
                      raw)
           r-classes (map #(keyword %) (set (map :rclass facts)))]
-
       (doseq [r-class r-classes]
         (println (str "Klasse: " r-class ))
-        (println "############################")
+        (println "----------------------------------------------------")
+        (println "drv  net bru rr results")
         (let [drivers (drivers-by-class r-class facts)
               driver-stats (map #(driver-stats % r-class facts) drivers)
-              sorted-by-bruto (sort-by :bruto > driver-stats)]
-          (doseq [item sorted-by-bruto]
-              (println (str (:driver item) " " (format "%03d" (:bruto item)) " " (:by-race item))))
-          (println)
-            )))))
+              sorted-by-netto (sort-by :netto > driver-stats)]
+          (doseq [item sorted-by-netto]
+             ;"drv net bru r results"
+            (println (str (:driver item) " " (format "%03d" (:netto item)) " " (format "%03d" (:bruto item)) " " (format "%02d" (:num-counting-races item)) " " (pr-str (:relevant-results item)))))
+          (println))))))
 
