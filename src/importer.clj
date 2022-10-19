75b2f9bf75bbc534d63ba7410b5a7f2df716d22f
Hans Westerbeek <hans@enhance-ict.com>
2022-10-19 22:01:17 +0200

    add best-effort importer

--- src/importer.clj
+++ src/importer.clj
@@ -1,2 +1,53 @@
-(ns importer)
+(ns importer
+  (:require [clojure.set :as cs]
+            [clojure.xml :as xml]
+            [refdata :as refdata])
+  (:import (java.io ByteArrayInputStream)))
+
+;(def interesting-tags #{:Class :Pos :PIC :Classified :Driver})
+;
+;(defn keep-interesting [result-tag]
+;  (filter
+;    (fn[subtag]
+;      (contains? interesting-tags (get subtag :tag)))
+;    result-tag))
+
+(defn content-from
+  [tag tags]
+  (:content
+    (first
+      (filter #(= tag (:tag %)) tags))))
+
+(def nutty-bool
+  {"YES" true
+   "NO" false})
+
+(defn -main
+  [& args]
+  (let [known-drivers (cs/map-invert (refdata/all-drivers (second *command-line-args*)))
+        race-id (nth args 2)
+        body (slurp (first *command-line-args*))
+        root (xml-seq (xml/parse (ByteArrayInputStream. (.getBytes body))))
+        items (map #(% :content) (filter (comp #{:Result} :tag) root))
+        ;items-stripped (map #(keep-interesting %) items)
+
+        lines (map
+               (fn [tags]
+                 (let [
+                       clazz (name (get refdata/class-aliases (clojure.string/lower-case (first (content-from :Class tags)))))
+                       pos (Integer/parseInt (first(content-from :PIC tags)))
+                       points (refdata/pos-to-points pos)
+                       classified (get nutty-bool (first(content-from :Classified tags)))
+                       driver (known-drivers (first (content-from :Name (content-from :Driver tags))))
+                       ]
+                   {:class clazz
+                    :points points
+                    :pos pos
+                    :classified classified
+                    :driver (if (nil? driver) "????" (name driver))}
+                   )
+                 )
+               items)]
+      (doseq [line lines]
+        (println (str race-id "," (:class line) "," (:driver line) "," (:pos line) "," (:points line))))))
 
--- src/refdata.clj
+++ src/refdata.clj
@@ -20,3 +20,43 @@
 
 (def scrap-reference {13 0, 14 1, 15 2, 16 3, 17 4, 18 4, 19 4,
                       20 4, 21 5, 22 5, 23 5, 24 6, 25 6, 26 6})
+
+(def class-aliases
+  {
+   "cay" :cayc
+   "bx" :boxc
+   "944" :944c
+   "rs" :rsc
+   "gt" :gtc
+   "gt4" :gt4c})
+
+(def points-reference ; very tempting to compute this but subject to too much change
+  {1 25
+   2 23
+   3 21
+   4 19
+   5 18
+   6 17
+   7 16
+   8 15
+   9 14
+   10 13
+   11 12
+   12 11
+   13 10
+   14 9
+   15 8
+   16 7
+   17 6
+   18 5
+   19 6
+   20 5
+   21 4
+   22 3
+   23 2
+   24 1})
+
+(defn pos-to-points [pos]
+  (if (< pos 24)
+    (get points-reference pos)
+    0))
